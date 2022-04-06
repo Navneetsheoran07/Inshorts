e7f8dc087ad0e33a5ab4126aa6efaa514252697a
beingmishra <rahul731999@gmail.com>
2022-04-06 23:06:43 +0530

    Initial commit

---
+++ app/src/main/java/com/programming/fever/inshorts/SliderItems.java
@@ -0,0 +1,14 @@
+package com.programming.fever.inshorts;
+
+public class SliderItems {
+
+    private String image;
+
+    public SliderItems(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
